a8eee89e68e405c5e92e883cb9f3bc558643895c
drunken_wizard <m.belhaj_zakelijk@hotmail.com>
2021-10-08 15:58:49 +0200

    u

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useEffect, useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonText, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
+import { SQLiteDBConnection } from '@capacitor-community/sqlite';
+import { sqlite, isJsonListeners } from '../App';
+import { useSQLite } from 'react-sqlite-hook/dist';
 
 const Tab3: React.FC = () => {
+  const [tests, setTests] = useState<any>([]);
+
+  useEffect(() => {
+    const initialize = async (): Promise<Boolean> => {
+      try {
+        let db: SQLiteDBConnection = await sqlite.createConnection("db_issue9");
+        await db.open();
+        let randomText = (Math.random() + 1).toString(36).substring(7);
+        await db.run("INSERT INTO test (name) VALUES (?)", [randomText]);
+        let res: any = await db.query("SELECT * FROM test");
+        setTests(res.values);
+        console.log(`query ${res}`);
+        await db.close();
+        return true;
+      }
+      catch (err) {
+        console.log(`Error: ${err}`);
+        return false;
+      }
+    }
+    initialize();
+  }, []);
   return (
     <IonPage>
       <IonHeader>
@@ -17,7 +42,15 @@ const Tab3: React.FC = () => {
             <IonTitle size="large">Tab 3</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+        {/* <ExploreContainer name="Tab 3 page" /> */}
+        <div className="container">
+          {tests.map((x: any, index: any) =>
+            <div key={index}><IonText>
+              {index} {x.name}
+            </IonText>
+            </div>
+          )}
+        </div>
       </IonContent>
     </IonPage>
   );
